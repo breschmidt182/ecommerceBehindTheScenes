376e87d952085a8185dc6dfa64896a830bf61589
Bre <breanna.schmidt12@gmail.com>
2022-07-02 18:24:19 -0400

    added foreign keys

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
 // Foo.hasOne(Bar, {
 //   onDelete: 'RESTRICT',
 //   onUpdate: 'RESTRICT'
@@ -13,20 +14,21 @@ const ProductTag = require('./ProductTag');
 // Categories have many Products
 Category.hasMany(Product, {
   onDelete: 'SET NULL',
-  onUpdate: 'CASCADE'
+  onUpdate: 'CASCADE',
 });
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  through: Category
+  through: 'Category',
+  foreignKey: 'category_id'
 });
 
 // Movie.belongsToMany(Actor, { through: 'ActorMovies' });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: ProductTag});
+Product.belongsToMany(Tag, {through: 'ProductTag', foreignKey: 'tag_id'});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: ProductTag});
+Tag.belongsToMany(Product, {through: 'ProductTag', foreignKey: 'tag_id' });
 
 module.exports = {
   Product,
